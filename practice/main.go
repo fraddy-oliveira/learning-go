c7578d5cdd790eb3412bc30bef03cb155f040ecc
Fraddy Oliveira <fraddy.dev@gmail.com>
2023-09-10 18:32:57 +0530

    feat: get name and age from input

--- practice/main.go
+++ practice/main.go
@@ -2,25 +2,74 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-var LANG = "GOLANG"
+var SERVER_ERROR = "Oops!! Some error occurred, Please try again"
 
 func main() {
-	getNameFromInput()
-}
-
-func getNameFromInput() {
-
-	reader := bufio.NewReader(os.Stdin)
-
 	fmt.Print("Enter your name: ")
 
-	inputName, _ := reader.ReadString('\n')
+	name, err := getName()
 
-	fmt.Printf("You have entered %s", inputName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("You have entered %s", name)
+
+	fmt.Print("Enter your age: ")
+
+	age, err := getAge()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("You have entered age %d\n", age)
+}
+
+func getName() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Print(err)
+
+		return "", errors.New(SERVER_ERROR)
+	}
+
+	return input, nil
+}
+
+func getAge() (int, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Print(err)
+
+		return 0, errors.New(SERVER_ERROR)
+	}
+
+	age, err := strconv.Atoi(strings.TrimSpace(input))
+
+	if err != nil {
+		log.Print(err)
+
+		return 0, errors.New("Entered age should be integer")
+	}
+
+	return age, nil
 }
 
 func variables() {
@@ -32,5 +81,5 @@ func variables() {
 
 	var defaultString string
 
-	fmt.Printf("default string value is %s and const is %s \n", defaultString, LANG)
+	fmt.Printf("default string value is %s \n", defaultString)
 }
