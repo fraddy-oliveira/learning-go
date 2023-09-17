8c7decf6c31e7908f5b1e5c072bd9e00d23ee5ed
Fraddy Oliveira <fraddy.dev@gmail.com>
2023-09-17 09:47:36 +0530

    feat: add calculator feature

--- practice/main.go
+++ practice/main.go
@@ -5,15 +5,73 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var SERVER_ERROR = "Oops!! Some error occurred, Please try again"
 
 func main() {
 	getYourInfo()
+}
+
+func calculator() {
+	fmt.Printf("Enter Value1: ")
+
+	value1, err := getCalculatorInput()
+
+	if err != nil {
+		panic(err)
+	}
+
+	floatValue1, err := strconv.ParseFloat(strings.TrimSpace(value1), 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Enter Value2: ")
+
+	value2, err := getCalculatorInput()
+
+	if err != nil {
+		panic(err)
+	}
+
+	floatValue2, err := strconv.ParseFloat(strings.TrimSpace(value2), 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Sum: %v\n", math.Round((floatValue1+floatValue2)*100)/100)
+}
+
+func getCalculatorInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Print(err)
+
+		return input, errors.New(SERVER_ERROR)
+	}
+
+	return input, nil
+}
+
+func numbers() {
+	i1, i2 := 2, 5
+
+	fmt.Println("Sum of numbers: ", (i1 + i2))
+
+	f1, f2, f3 := 1.3999, 2.899, 5.99999
+
+	fmt.Printf("Sum of floats: %.2f\n", math.Round((f1+f2+f3)*100)/100)
 }
 
 func getYourInfo() {
@@ -38,6 +96,14 @@ func getYourInfo() {
 	}
 
 	fmt.Printf("You have entered age %d\n", age)
+
+	infoTime := time.Now()
+
+	fmt.Printf("This info was printed at %s\n", infoTime.Format(time.ANSIC))
+
+	parsedTime, _ := time.Parse(time.ANSIC, "Sun Sep 17 02:25:20 2023")
+
+	fmt.Printf("This is parsing time from info %v\n", parsedTime)
 }
 
 func getName() (string, error) {
